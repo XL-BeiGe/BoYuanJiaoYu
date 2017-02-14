2da9bf35fe6748b7564370bab39e6880066b10f3
xiaolang <langyaoyu@hotmail.com>
2017-02-14 11:28:57 +0800

    啦啦啦

---
+++ BoYuanJiaoYu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoYuanJiaoYu
+//
+//  Created by newmac on 2017/2/14.
+//  Copyright © 2017年 BeiGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoYuanJiaoYu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BoYuanJiaoYu
+//
+//  Created by newmac on 2017/2/14.
+//  Copyright © 2017年 BeiGe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
